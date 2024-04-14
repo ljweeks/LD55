4db64fe619d1251a1c2d3bffe05c28a975e7053e
lexweeks <lexweeks@gmail.com>
2024-04-14 15:35:20 -0500

    resized fella

--- title.gd
+++ title.gd
@@ -10,5 +10,6 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if(Input.is_action_just_pressed("look")):
-		get_tree().change_scene_to_packed(scene)
+		var scene2 = load("res://main.tscn")
+		get_tree().change_scene_to_packed(scene2)
 	pass

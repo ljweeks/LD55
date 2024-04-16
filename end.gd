7fc491218990f06bccfdd2e57ea1323fc7dfa75e
Josie H <57545113+josiezxh@users.noreply.github.com>
2024-04-15 19:01:49 -0500

    added highest score tracking

--- end.gd
+++ end.gd
@@ -3,12 +3,18 @@ extends Node2D
 @onready var minLable = $ScoreBoard/VBoxContainer/Panel/min
 @onready var secLable = $ScoreBoard/VBoxContainer/Panel/sec
 @onready var fellaLable = $ScoreBoard/VBoxContainer/VBoxContainer2/fellacount
+@onready var maxminLable = $HighScoreBoard/VBoxContainer/Panel/min
+@onready var maxsecLable = $HighScoreBoard/VBoxContainer/Panel/sec
+@onready var maxfellaLable = $HighScoreBoard/VBoxContainer/VBoxContainer2/fellacount
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	minLable.text = str(Scoring.cur_min)
 	secLable.text = str(Scoring.cur_sec)
 	fellaLable.text = str(Scoring.cur_count)
+	maxminLable.text = str(Scoring.max_min)
+	maxsecLable.text = str(Scoring.max_sec)
+	maxfellaLable.text = str(Scoring.max_count)
 
 #@onready var scene = preload("res://main.tscn")
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- scoring.gd
+++ scoring.gd
@@ -9,9 +9,21 @@ var max_sec
 var max_count
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	cur_count = 0
+	cur_min = 0
+	cur_sec = 0
+
+	max_min = 0
+	max_sec = 0
+	max_count = 0
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if cur_count > max_count:
+		max_count = cur_count
+	if cur_min >= max_min:
+		max_min = cur_min
+		if cur_sec > max_min:
+			max_sec = cur_sec
